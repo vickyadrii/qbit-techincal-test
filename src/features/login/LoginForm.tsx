a05824e61ffdb702d9ae1adcf9088132e34782f1
vickyadri29 <vickyadri290501@gmail.com>
2024-05-14 14:22:25 +0800

    chore(auth): added title on login form

--- src/features/login/LoginForm.tsx
+++ src/features/login/LoginForm.tsx
@@ -34,39 +34,42 @@ const LoginForm = () => {
   return (
     <div className="max-w-2xl m-auto p-5 flex justify-center items-center h-screen">
       <div className="bg-white p-20 w-full rounded-md">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleOnSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input type="text" placeholder="Username..." {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+        <div className="flex flex-col gap-6">
+          <h2 className="text-center text-2xl font-semibold">QBIT Technical Test</h2>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleOnSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="Username..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="Password..." {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Password..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </div>
   );
